3b02d3ad023e129c41c0b2c87c42ffaa718df880
Joellion32 <joeltoribiopolanco@gmail.com>
2020-07-24 04:39:34 -0400

    search countries by added name

--- server/models/ClientModel.js
+++ server/models/ClientModel.js
@@ -43,7 +43,8 @@ type: Number
 status: {
 type: Boolean,
 default: false   
-}
+},
+
 });
 
 // not seed password
--- server/routes/CountryRoutes.js
+++ server/routes/CountryRoutes.js
@@ -23,10 +23,9 @@ app.get('/get/countries', (req, res) => {
 });
 
 
-// view country for id
+// view country by id
 app.get('/view/countries/:id', (req, res) => {
 let id = req.params.id;
-console.log(id)
 CountryModel.findById(id, (err, countryDB) => {
     if (err) {
        return res.status(500).json({
@@ -49,6 +48,34 @@ CountryModel.findById(id, (err, countryDB) => {
     });
 });
 });
+
+
+// view country by name
+app.get('/view/country/:name', (req, res) => {
+    let name = req.params.name;
+    
+    CountryModel.find({country: name}, (err, countryDB) => {
+        if (err) {
+           return res.status(500).json({
+                ok: false,
+                message: 'An error ocurred',
+                error: err
+            });
+        }
+    
+        if (!countryDB) {
+            return res.status(400).json({
+                ok: false,
+                message: 'this country not exist',
+            });
+        }
+               
+        res.json({
+            ok: true,
+            country: countryDB
+        });
+    });
+    });
 
 // save countries
 app.post('/countries/save', (req, res) => {
